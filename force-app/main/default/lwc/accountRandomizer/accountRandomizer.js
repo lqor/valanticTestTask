df5755e7a8f9a88fbd8179c45138da852c602b53
igor.kudryk@empaua.com <igor.kudryk@empaua.com>
2022-08-04 12:26:14 +0200

    String random generator integration

--- force-app/main/default/lwc/accountRandomizer/accountRandomizer.js
+++ force-app/main/default/lwc/accountRandomizer/accountRandomizer.js
@@ -2,18 +2,59 @@ import { LightningElement, wire, api } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 
+import doStringRandomizerCallout from '@salesforce/apex/StringGeneratorCallout.doStringRandomizerCallout';
+
 export default class AccountRandomizer extends LightningElement {
     accountFields = [NAME_FIELD];
+
     @api recordId;
-    clickedButtonLabel = '';
+    @api objectApiName;
+
+    randomName;
+    isTrigger;
+    error;
+    isLoading;
 
     @wire( getRecord, { 
-            recordId: '001456789012345678', 
+            recordId: '$recordId', 
             fields: ['$accountFields']
     }) account;
 
+    get name() {
+        return getFieldValue(this.account.data, NAME_FIELD);
+    }
+
     handleRandomized(event) {
-        console.log(event.target.label);
-        this.clickedButtonLabel = event.target.label;
+        this.isLoading = true;
+
+        doStringRandomizerCallout({ accountId: this.recordId })
+            .then(result => {
+                this.randomName = result;
+            })
+            .catch(error => {
+                this.error = error;
+            })
+            .finally(() => {
+                this.isLoading = false;
+            })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();     
+
+        console.log(this.isTrigger);
+        if(!this.isTrigger) {
+            return;
+        }
+
+        const fields = event.detail.fields;
+        fields.Name = this.randomName;
+        
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+        this.toggleCheckbox();
+    }
+
+    toggleCheckbox() {
+        this.isTrigger = !this.isTrigger;
     }
 }
